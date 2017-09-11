5b6e207f4d71a976010831b942b91e6db3f47449
Yashwant Vaishnav <vaishnav.yashwant1989@gmail.com>
2017-09-11 19:56:39 +0530

    resizable containers using Angular2 Split

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,18 +2,17 @@
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Ng2BootstrapModule } from 'ng2-bootstrap';
+import { SplitPaneModule } from 'ng2-split-pane/lib/ng2-split-pane';
 
 import { DashboardComponent } from './dashboard.component';
-
-
-
 
 
 @NgModule({
     imports: [
         CommonModule,
       RouterModule,
-      Ng2BootstrapModule.forRoot(),     
+      Ng2BootstrapModule.forRoot(),
+      SplitPaneModule
     ],
     declarations: [DashboardComponent],
     exports: [DashboardComponent]
