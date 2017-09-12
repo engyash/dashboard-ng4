e2ed6665a9dd8405428ea8630b036a9cdb545988
Yashwant Vaishnav <vaishnav.yashwant1989@gmail.com>
2017-09-12 10:57:11 +0530

    sorting items and added animation effect

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Ng2BootstrapModule } from 'ng2-bootstrap';
 import { SplitPaneModule } from 'ng2-split-pane/lib/ng2-split-pane';
-
+import { SortablejsModule, SortablejsOptions } from 'angular-sortablejs';
 import { DashboardComponent } from './dashboard.component';
 
 
 @NgModule({
     imports: [
         CommonModule,
-      RouterModule,
-      Ng2BootstrapModule.forRoot(),
-      SplitPaneModule
+        RouterModule,
+        Ng2BootstrapModule.forRoot(),
+        SplitPaneModule,
+        SortablejsModule
     ],
     declarations: [DashboardComponent],
     exports: [DashboardComponent]
